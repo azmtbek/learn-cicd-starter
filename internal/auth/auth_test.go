e9c7e5a8d31a8b25815bbd054ecb287a11e8f901
Azamatbek Mamarajabov <azamatbek.dev@gmail.com>
2025-05-13 02:13:45 +0500

    add error test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+	"github.com/google/go-cmp/cmp"
+	"errors"
+)
+
+
+func TestGetAPIKey(t *testing.T) {
+	tests := map[string]struct {
+		headers http.Header
+		want string
+		err error
+	}{
+		"simple": {headers: http.Header{"Authorization": {"ApiKey key"}}, want: "key", err: nil},
+		"errorNoAuth": {headers: http.Header{"Auth": {"Key key"}}, want: "", err: ErrNoAuthHeaderIncluded },
+		"error Header": {headers: http.Header{"Auth": {"ApiKey key"}}, want: "key", err: nil},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.headers)
+			diff := cmp.Diff(tc.want, got)
+			errIs := errors.Is(tc.err, err)
+			if diff != "" {
+				t.Fatalf(diff)
+			}
+			if !errIs {
+				t.Error(err)
+			}
+		})
+	}
+}
